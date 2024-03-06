1843e89d4da3d612a4c013bfbc28347bedd33be0
Dante <bazandante05@gmail.com>
2024-03-05 23:59:44 -0300

    first commit

---
+++ js/metodosDeString.js
@@ -0,0 +1,116 @@
+// METODOS DE STRING
+
+//LENGTH
+
+// let myName = "Dante";
+// console.log(myName.length);
+
+//toUpperCase o toLowerCase
+
+// let theBest = "Messi es el mejor jugador de todos los tiempos";
+
+// console.log(theBest.toUpperCase());
+// console.log(theBest.toLowerCase());
+
+// replace
+
+// let theBest = "Messi es el mejor jugador de todos los tiempos";
+// console.log(theBest);
+
+// let elNuevoBest = theBest.replace("Messi", "Maradona");
+// console.log(elNuevoBest);
+
+// replaceAll
+
+// let texto =
+//   "Messi es el mejor futbolista del mundo porque Messi sabe cuanto pesa la del mundo";
+// console.log(texto);
+
+// let nuevoTexto = texto.replaceAll("Messi", "Maradona");
+// console.log(nuevoTexto);
+
+//indexof
+
+// console.log("Lionel Messi".indexOf("Messi"));
+
+// trim - Quita Espacios // ACLARACION: Solo elimina los espacios del comienzo y el final,no los de entre medio.
+
+// let texto = "     Lionel Andres     ";
+// console.log(texto);
+
+// let textoSinEspacios = texto.trim();
+// console.log(textoSinEspacios);
+
+// split() // lo convierte en un array
+
+// let saludo = "Hola-Mundo";
+// let nuevoSaludo = saludo.split("-");
+// console.log(nuevoSaludo);
+
+// typeOf // Determina el tipo de dato
+
+// let miFuncion = new Function("5+2");
+// let color = "Rojo";
+// let altura = 3;
+// let esCasado = true;
+// console.log(typeof miFuncion);
+// console.log(typeof color);
+// console.log(typeof altura);
+// console.log(typeof esCasado);
+
+// repeat
+
+// let nombre = "Dante ";
+// console.log(nombre);
+// let nombreRepetido = nombre.repeat(3);
+// console.log(nombreRepetido);
+
+// charAt // Nos permite obtener el caracter de la posicion index que le indiquemos por parametro
+
+// let nombre = "Dante";
+// console.log(nombre.charAt(4));
+//muestra e en consola
+
+// concat
+
+// let frase = "Dante es un ";
+// let fraseCompleta = frase.concat("nasheee");
+// console.log(fraseCompleta);
+
+// startsWith // solo retorna true o false
+
+// let texto = "Hola Mundo";
+// console.log(texto.startsWith("Hola"));
+// console.log(texto.startsWith("Mundo"));
+// console.log(texto.startsWith("hola")); // case sensitive
+
+// endsWith // solo retorna true o false
+
+// let texto = "Hola Mundo";
+// console.log(texto.endsWith("mundo"));
+// console.log(texto.endsWith("Mundo"));
+// console.log(texto.endsWith("Hola"));
+
+// includes
+
+// let nombre = "Dante";
+// const array = [1, 2, 3, 4];
+
+// console.log(nombre.includes("D"));
+// console.log(nombre.includes("E"));
+
+// console.log("-----");
+
+// console.log(array.includes(3));
+// console.log(array.includes(7));
+
+// slice
+
+// let aclaracion = "Messi es mil veces mejor que Maradona";
+
+// console.log(aclaracion.slice(0, 24)); //consolea "Messi es mil veces mejor"
+
+//PRACTICAS
+
+// let comida = "papas fritas ";
+// console.log(comida.concat("con cheddar"));
